3293f508c7c4712fc25b919c47bd22f6385b93e3
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-03-18 11:27:03 -0600

    web3 setup

--- lottery-client/src/App.js
+++ lottery-client/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import web3 from './web3';
+
 import './App.css';
 
 function App() {
+  web3.eth.getAccounts().then(console.log);
+
   return (
     <div className="App">
       <header className="App-header">
