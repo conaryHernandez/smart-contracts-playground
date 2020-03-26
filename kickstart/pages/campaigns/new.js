f41b3e8c854d7790901025c0bb4192253572d996
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-03-25 19:02:57 -0600

    form error handling button spinners

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
+import { Button, Input, Form, Message } from 'semantic-ui-react';
+
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
+  state = {
+    minimunContribution: '',
+    errorMessage: '',
+    loading: false
+  };
+
+  onSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true, errorMessage: '' });
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimunContribution)
+        .send({
+          from: accounts[0]
+        });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+
+    this.setState({ loading: false });
+  };
+
   render() {
     return (
       <Layout>
-        <h1> New Campaign!</h1>
+        <h3>Create a Campaign</h3>
+
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+          <Form.Field>
+            <label>Minimun Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimunContribution}
+              onChange={e =>
+                this.setState({ minimunContribution: e.target.value })
+              }
+            />
+          </Form.Field>
+
+          <Button loading={this.state.loading} primary>
+            Create
+          </Button>
+
+          <Message error header="Oops!" content={this.state.errorMessage} />
+        </Form>
       </Layout>
     );
   }
