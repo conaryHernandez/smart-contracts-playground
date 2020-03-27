aeb92c83b8ad6b9c2c479e9c86dc3f1a73caf7c5
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-03-27 15:23:36 -0600

    customcard groups

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
+import { Card } from 'semantic-ui-react';
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
-    console.log(props.query.address);
+    const campaign = Campaign(props.query.address);
+    const summary = await campaign.methods.getSummary().call();
 
-    return {};
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4]
+    };
+  }
+
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of Manager',
+        description:
+          'The manager created this campaign and can create request to withdraw money',
+        style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (wei)',
+        description:
+          'You must contribute at least this much wei to be a contribuitor'
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of requests',
+        description:
+          'A request tries to withdraw money from the contract. Request must be aprove by approvers'
+      }
+    ];
+
+    return <Card.Group items={items} />;
   }
 
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
